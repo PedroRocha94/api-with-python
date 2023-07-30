6020ff2ec51ec0703e2ae85ae6c81fb18e9c2060
Pedro Rocha <pedrorocha73658@gmail.com>
2023-07-30 11:04:12 -0300

    Creating endpoint to delete book.

--- app.py
+++ app.py
@@ -46,6 +46,16 @@ def editar_livro_por_id(id):
 def incluir_novo_livro():
   novo_livro = request.get_json()
   livros.append(novo_livro)
+  
+  return jsonify(livros)
+
+#Deletar
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+  for indice,livro in enumerate(livros):
+    if livro.get('id') == id:
+      del livros[indice]
+
   return jsonify(livros)
 
 app.run(port=5000, host='localhost', debug=True)
